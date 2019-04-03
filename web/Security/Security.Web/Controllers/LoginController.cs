37b13db458add7f3601f2a831c0c6e5aab87586f
rnota <rnota@gruposion.bo>
2019-04-03 18:20:06 -0400

    * add consume api web service

--- web/Security/Security.Web/Controllers/LoginController.cs
+++ web/Security/Security.Web/Controllers/LoginController.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Security.Web.Models;
 
 namespace Security.Web.Controllers
 {
@@ -18,31 +23,92 @@ namespace Security.Web.Controllers
             return View();
         }
 
+        [NonAction]
+        public HttpClient Initial()
+        {
+            var Client = new HttpClient
+            {
+                BaseAddress = new Uri(Utils.Url_Service)
+            };
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return Client;
+        }
+
+        [NonAction]
+        public void Api_authentication(HttpClient client)
+        {
+            var byteArray = Encoding.ASCII.GetBytes(Utils.AuthUser + ":" + Utils.AuthPass);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+        }
+
+
         [HttpPost, AllowAnonymous, ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(string returnUrl, string username, string password)
         {
-            if (username == "rnota" && password == "mercado.nota")
+            try
             {
-                var claims = new List<Claim>
+
+                HttpClient client = this.Initial();
+                this.Api_authentication(client);
+
+                Dictionary<string, string> dict = new Dictionary<string, string>
                 {
-                    new Claim(ClaimTypes.Name, "raul", ClaimValueTypes.String, "https://soe.mis.mod13.com")
+                    { "usuario", username },
+                    { "password", password },
                 };
 
-                var userIdentity = new ClaimsIdentity(claims, "SecureLogin");
-                var userPrincipal = new ClaimsPrincipal(userIdentity);
+                StringBuilder builder = new StringBuilder();
 
-                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal,
-                    new AuthenticationProperties
+                foreach (KeyValuePair<string, string> pair in dict)
+                {
+                    builder.Append(pair.Key).Append("=").Append(pair.Value).Append('&');
+                }
+
+                string queryString = builder.ToString();
+                // Remove the final delimiter
+                queryString = queryString.TrimEnd('&');
+
+                HttpRequestMessage requestMessage = new HttpRequestMessage
+                {
+                    RequestUri = new Uri(client.BaseAddress + "user_validate?" + queryString),
+                    Method = HttpMethod.Get
+                };
+
+                HttpResponseMessage response = client.SendAsync(requestMessage).Result;
+                var result = response.Content.ReadAsStringAsync().Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    bool isOk = JsonConvert.DeserializeObject<bool>(result);
+
+                    if (isOk)
                     {
-                        ExpiresUtc = DateTime.UtcNow.AddMinutes(20),
-                        IsPersistent = false,
-                        AllowRefresh = false
-                    });
+                        var claims = new List<Claim>{
+                            new Claim(ClaimTypes.Name, "raul", ClaimValueTypes.String, "https://soe.mis.mod13.com")
+                        };
 
-                return GoToReturnUrl(returnUrl);
+                        var userIdentity = new ClaimsIdentity(claims, "SecureLogin");
+                        var userPrincipal = new ClaimsPrincipal(userIdentity);
+
+                        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal,
+                            new AuthenticationProperties
+                            {
+                                ExpiresUtc = DateTime.UtcNow.AddMinutes(20),
+                                IsPersistent = false,
+                                AllowRefresh = false
+                            });
+
+                        return GoToReturnUrl(returnUrl);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
 
-            return RedirectToAction("Index","Login");
+            return RedirectToAction("Index", "Login");
         }
 
         [AllowAnonymous]
---
+++ web/Security/Security.Web/Models/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Security.Web.Models
+{
+    public class Utils
+    {
+        public const string Url_Service = "http://localhost:9000/";
+
+        public const string AuthUser = "rnota";
+        public const string AuthPass = "mercado.nota";
+    }
+}
